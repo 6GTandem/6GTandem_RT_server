6b3ef18e4c6e672c28d5c2e1c15480e2af4c1b98
Tijl <tijl.schepens@kuleuven.be>
2026-01-09 15:09:09 +0100

    Adding support to generate UEs in user defined zones with x,y,x coordiantes. Adding the arena environment.

--- src/ue_locations_generator.py
+++ src/ue_locations_generator.py
@@ -31,49 +31,85 @@ def create_user_location_dataset(config, logger, dataset_name, x_pos, y_pos):
     else:
         logger.info(f"Dataset not found. Creating new dataset at {file_name}")
 
-        # grid under each RU
+        # grid under each RU 
+        N_RUs = config['stripe_config']['N_RUs']# adjust to size of the room (along y axis)
+        N_stripes = config['stripe_config']['N_stripes'] # adjust to size of the room (alang x axis)
         stripe_direction = config["stripe_config"]["stripe_direction"]
-        N_RUs = config["stripe_config"]["N_RUs"]  # adjust to size of the room (along y axis)
-        N_stripes = config["stripe_config"]["N_stripes"]  # adjust to size of the room (alang x axis)
-        space_between_stripes = config["stripe_config"]["space_between_stripes"]  # in meters
+        space_between_stripes = config['stripe_config']['space_between_stripes'] # in meters
         ue_config = config["ue_locations_config"]
-        ue_points = ue_config["num_locations"]
-        simulation_area = ue_config["ue_area"]
-        safety_offset = ue_config["safety_offset"]
-        z_height = ue_config["z_height"]
+        ue_zones = ue_config.get("zones", None)
         objects = config["objects"]
 
-        x_points = np.random.uniform(simulation_area[0] + safety_offset, simulation_area[1] - safety_offset, ue_points)
-        y_points = np.random.uniform(simulation_area[2] + safety_offset, simulation_area[3] - safety_offset, ue_points)
-        samples = np.column_stack((x_points, y_points, z_height * np.ones(ue_points)))
+        if ue_zones is None:
+            ue_points = ue_config['num_locations']
+            simulation_area = ue_config["ue_area"]
+            safety_offset = ue_config["safety_offset"]
+            z_height = ue_config["z_height"]
 
-        nr_ue_locs = ue_points + (N_RUs * N_stripes)
-        logger.info(f"Total samples: {nr_ue_locs}")
+            x_points = np.random.uniform(simulation_area[0] + safety_offset, simulation_area[1]  - safety_offset, ue_points)
+            y_points = np.random.uniform(simulation_area[2] + safety_offset, simulation_area[3] - safety_offset, ue_points)
+            samples = np.column_stack((x_points, y_points, z_height * np.ones(ue_points)))
 
-        # generate dataset of ue locations
-        samples_grid = np.zeros((N_RUs * N_stripes, 3))
-        stripe_labels = []
-        ru_labels = []
-        for stripe_idx in range(N_stripes):
-            for RU_idx in range(N_RUs):
-                # compute RU position
-                rux = x_pos[RU_idx]
-                ruy = y_pos[RU_idx]
+            nr_ue_locs = ue_points + (N_RUs * N_stripes)
 
-                if stripe_direction == "y":
-                    rux += stripe_idx * space_between_stripes
-                elif stripe_direction == "x":
-                    ruy += stripe_idx * space_between_stripes
-                else:
-                    raise ValueError(f"Invalid stripe direction: {stripe_direction}")
+            # generate dataset of ue locations
+            samples_grid = np.zeros((N_RUs * N_stripes, 3))
+            stripe_labels = []
+            ru_labels = []
+            for stripe_idx in range(N_stripes):
+                for RU_idx in range(N_RUs):
+                    # compute RU position
+                    rux = x_pos[RU_idx]
+                    ruy = y_pos[RU_idx]
 
-                samples_grid[stripe_idx * N_RUs + RU_idx, :] = [rux, ruy, z_height]
-                stripe_labels.append(stripe_idx)
-                ru_labels.append(RU_idx)
+                    if stripe_direction == "y":
+                        rux += stripe_idx * space_between_stripes
+                    elif stripe_direction == "x":
+                        ruy += stripe_idx * space_between_stripes
+                    else:
+                        raise ValueError(f"Invalid stripe direction: {stripe_direction}")
 
-        # Combine samples into a single array
-        all_samples = np.vstack([samples, samples_grid])
+                    samples_grid[stripe_idx * N_RUs + RU_idx, :] = [rux, ruy, z_height]
+                    stripe_labels.append(stripe_idx)
+                    ru_labels.append(RU_idx)
+
+            # Combine samples into a single array
+            all_samples = np.vstack([samples, samples_grid])
+
+            # Additional boolean to check if the ue is under the stripe grid or in a zone.
+            ue_on_stripe_grid = np.array([False] * samples.shape[0] + [True] * samples_grid.shape[0])
+
+            stripe_labels = np.array([np.nan] * samples.shape[0] + stripe_labels)
+            ru_labels = np.array([np.nan] * samples.shape[0] + ru_labels)
+        else:
+            samples = []
+
+            for zone in ue_zones.values():
+                bounds = zone["bounds"]
+                steps = zone["steps"]
+                cols = zone["cols"]
+                rows = zone["rows"]
+
+                x_points = np.linspace(bounds[0], bounds[1], cols)
+                y_points = np.linspace(bounds[2], bounds[3], rows)
+                z_points = np.linspace(bounds[4], bounds[5], rows)
+
+                x_rep = np.repeat(x_points, len(y_points))
+                y_tile = np.tile(y_points, len(x_points))
+                z_rep = np.tile(z_points, cols)
+
+                samples.append(np.column_stack((x_rep, y_tile, z_rep)))
+            
+            all_samples = np.concatenate(samples)
+            # Additional boolean to check if the ue is under the stripe grid or in a zone.
+            ue_on_stripe_grid = np.array([False] * all_samples.shape[0])
+
+            stripe_labels = np.array([np.nan] * all_samples.shape[0])
+            ru_labels = np.array([np.nan] * all_samples.shape[0])
+
+
         nr_ue_locs = all_samples.shape[0]
+        logger.info(f"Total samples: {nr_ue_locs}")
 
         invalid_point_labels = []
         for s in all_samples:
@@ -81,14 +117,6 @@ def create_user_location_dataset(config, logger, dataset_name, x_pos, y_pos):
             if invalid_point:
                 logger.info(f"Point {s} - point outside of zone.")
             invalid_point_labels.append(invalid_point)
-
-        zone_labels = np.array(["Zone 1"] * samples.shape[0] + ["Grid"] * samples_grid.shape[0])
-
-        # Additional boolean to check if the ue is under the stripe grid or in a zone.
-        ue_on_stripe_grid = np.array([False] * samples.shape[0] + [True] * samples_grid.shape[0])
-
-        stripe_labels = np.array([np.nan] * samples.shape[0] + stripe_labels)
-        ru_labels = np.array([np.nan] * samples.shape[0] + ru_labels)
 
         # unique id per user
         user_ids = np.arange(nr_ue_locs)
@@ -100,7 +128,6 @@ def create_user_location_dataset(config, logger, dataset_name, x_pos, y_pos):
                 "x": ("user", all_samples[:, 0].astype(np.float32)),
                 "y": ("user", all_samples[:, 1].astype(np.float32)),
                 "z": ("user", all_samples[:, 2].astype(np.float32)),
-                "zone": ("user", zone_labels),
                 "ue_stripe_idx": ("user", stripe_labels),
                 "ue_ru_idx": ("user", ru_labels),
                 "ue_on_stripe_grid": ("user", ue_on_stripe_grid),
